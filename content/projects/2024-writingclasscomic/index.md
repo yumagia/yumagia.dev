468fb99fc21b23c1bf309cf4571f71812401b488
gao00515 <gao00515@umn.edu>
2024-12-11 01:31:36 -0600

    Added new project for my writing class

---
+++ content/projects/2024-writingclasscomic/index.md
@@ -0,0 +1,17 @@
++++
+title = "UNIVERSITY WRITING ARTIFACT" 
+description = "A group research project for my university writing class. I illustrated a comic about our research for the final class presentation."
+date = 2024-12-10
+[extra]
+start_date = "10 DEC 2024"
+status = "CURRENT"
+icon = "/projects/2024-writingclasscomic/IMG_3427.PNG"
++++
+
+{{ webcomic() }}
+
+
+<script src="{{ webcomic.js }}"></script>
+
+
+{{ gallery() }}
---
+++ static/js/webcomic.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const images = document.querySelectorAll('.webcomic-slider img');
+    let currentIndex = 0;
+
+    const showImage = (index) => {
+        images.forEach((image, i) => {
+            image.style.display = i === index ? 'block' : 'none';
+        });
+    };
+
+    document.querySelector('.next').addEventListener('click', () => {
+        currentIndex = (currentIndex + 1) % images.length;
+        showImage(currentIndex);
+    });
+
+    document.querySelector('.prev').addEventListener('click', () => {
+        currentIndex = (currentIndex - 1 + images.length) % images.length;
+        showImage(currentIndex);
+    });
+
+    showImage(currentIndex);
+});
